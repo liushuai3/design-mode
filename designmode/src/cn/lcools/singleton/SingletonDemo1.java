855d0945b155cabc8919620181723a2cc00446a7
liushuai3 <liushuai3@asiainfo.com>
2019-05-15 16:18:02 +0800

    单例模式，饿汉式实现

---
+++ designmode/src/cn/lcools/singleton/SingletonDemo1.java
@@ -0,0 +1,14 @@
+package cn.lcools.singleton;
+
+/**
+ * Created by liushuai on 2019/5/15.
+ * 单例模式，饿汉式实现
+ *
+ */
+public class SingletonDemo1 {
+    private static SingletonDemo1 instance = new SingletonDemo1();
+    private SingletonDemo1(){}
+    public static SingletonDemo1 getInstance(){
+        return instance;
+    }
+}
