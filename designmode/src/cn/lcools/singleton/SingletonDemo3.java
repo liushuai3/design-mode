5fcceff6f54c3ec49382bb6f41b5580fabceb076
liushuai3 <liushuai3@asiainfo.com>
2019-05-15 16:32:19 +0800

    单例模式，静态内部类实现

---
+++ designmode/src/cn/lcools/singleton/SingletonDemo3.java
@@ -0,0 +1,16 @@
+package cn.lcools.singleton;
+
+/**
+ * Created by liushuai on 2019/5/15.
+ * 单例模式，静态内部类实现
+ *
+ */
+public class SingletonDemo3 {
+    private static class SingletonInstance{
+        private static final SingletonDemo3 instance = new SingletonDemo3();
+    }
+    private SingletonDemo3(){}
+    public static SingletonDemo3 getInstance(){
+        return SingletonInstance.instance;
+    }
+}
